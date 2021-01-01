9f6ec41be3cf2d1280adf35f92b7a375e915d533
Heath Ivie <hivie@seamgen.com>
2020-12-31 21:54:28 -0800

    Update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cp ./README.md ./dist/README.md
+cp ./package.json ./dist/package.json
+cp ./package-lock.json ./dist/package-lock.json
 browserify ./src/index.js > ./dist/main.js
 terser ./dist/main.js --compress --mangle -o ./dist/index.min.js 
 rm ./dist/main.js
